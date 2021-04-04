35931556760c62554980ece81d556e0478556d83
Ruan <martinelliruan@gmail.com>
2021-04-04 23:10:08 +0200

    fix terraform backend config

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">= 0.13"
 
-  backend "gcs" {
-    bucket = "tfstate-my-cool-project-9981"
-  }
+  # backend "gcs" {
+  #   bucket = "tfstate-PROJECT_ID"
+  # }
 
   required_providers {
     google = ">= 3.3"
